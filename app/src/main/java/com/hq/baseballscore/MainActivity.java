657c56ca4bbde59aefb4be99c18dbeb33917fd51
Reinaldo Molina <rmolin88@gmail.com>
2016-09-20 15:56:08 -0400

    init commit

---
+++ app/src/main/java/com/hq/baseballscore/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hq.baseballscore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_scrolling);
+    }
+}
