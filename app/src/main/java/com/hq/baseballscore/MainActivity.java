2c79a250951b0f82ccf06d2b1d379270d9459132
Reinaldo Molina <rmolin88@gmail.com>
2016-09-20 23:36:41 -0400

    new pragmatic version ready to be tested

--- app/src/main/java/com/hq/baseballscore/MainActivity.java
+++ app/src/main/java/com/hq/baseballscore/MainActivity.java
@@ -1,45 +1,41 @@
 package com.hq.baseballscore;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.StringBuilderPrinter;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
   private static final String TAG = "BaseballScoreApp";
   private static final int NumInnings = 9;
+  private static final int NumStats = 3;
+  private static final int InningTextSize = 64;
+  private static final String[] sInningStatsNames = {"Ball", "Strike", "Out"};
 
-  private static int iBalls = 0;
-  private static int iStrikes = 0;
-  private static int iOuts = 0;
+  private static int[] iaGuestInningStats = new int[NumStats];
+  private static int[] iaHomeInningStats = new int[NumStats];
 
-  // Team scores
-  private static int iGuestRuns = 0;
-  private static int iGuestHits = 0;
-  private static int iGuestErrors = 0;
+  private static int[] iaGuestGameStats = new int[NumStats];
+  private static int[] iaHomeGameStats = new int[NumStats];
 
-  private static int iHomeRuns = 0;
-  private static int iHomeHits = 0;
-  private static int iHomeErrors = 0;
-
-  // Inning Scores
   private static int[] iaGuestInningRuns = new int[NumInnings];
   private static int[] iaHomeInningRuns = new int[NumInnings];
 
-  private TextView tvBalls;
-  private TextView tvStrikes;
-  private TextView tvOuts;
+  private static final float llInningsStatsWeight = 10/6;
+  private LinearLayout llInningStats;
+  private TextView[] tvaInningsStats = new TextView[NumStats*2];
 
-  private TextView tvGuestRuns;
-  private TextView tvGuestHits;
-  private TextView tvGuestErrors;
-
-  private TextView tvHomeRuns;
-  private TextView tvHomeHits;
-  private TextView tvHomeErrors;
+  private TextView[] tvaGuestGameStats = new TextView[NumInnings];
+  private TextView[] tvaHomeGameStats = new TextView[NumInnings];
 
   private TextView[] tvaGuestInningsRuns = new TextView[NumInnings];
   private TextView[] tvaHomeInningsRuns = new TextView[NumInnings];
@@ -48,11 +44,11 @@ public class MainActivity extends AppCompatActivity {
     public boolean onTouch(View view, MotionEvent motionEvent) {
       if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
         // TODO Convert this down here to a function that decodes who got called
-        if (view.getId() == tvBalls.getId()) {
-          Log.i(TAG, view.getId() + " - Button Pressed!");
-          iBalls++;
-          tvBalls.setText("" + iBalls);
-        }
+        // if (view.getId() == tvBalls.getId()) {
+        // Log.i(TAG, view.getId() + " - Button Pressed!");
+        // iBalls++;
+        // tvBalls.setText("" + iBalls);
+        // }
       }
       return false;
     }
@@ -62,43 +58,48 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.content_scrolling);
 
-    // Initialize all TextViews with their listeners
-    tvBalls = (TextView) findViewById(R.id.tvBallsValue);
-    tvBalls.setOnTouchListener(new myTouchListener());
+    llInningStats = (LinearLayout) findViewById(R.id.llInningStats);
 
-    tvStrikes = (TextView) findViewById(R.id.tvStrikesValue);
-    tvStrikes.setOnTouchListener(new myTouchListener());
+    { // Set up Inning Stats
+      LinearLayout.LayoutParams llparamsInningStats = new LinearLayout.LayoutParams(0,
+              LinearLayout.LayoutParams.MATCH_PARENT, llInningsStatsWeight);
 
-    tvOuts = (TextView) findViewById(R.id.tvOutsValue);
-    tvOuts.setOnTouchListener(new myTouchListener());
 
-    tvGuestRuns = (TextView) findViewById(R.id.tvGuestRuns);
-    tvGuestRuns.setOnTouchListener(new myTouchListener());
+      for (int k=0; k < NumStats; k++) {
+        tvaInningsStats[k].setLayoutParams(llparamsInningStats);
+        tvaInningsStats[k+1].setLayoutParams(llparamsInningStats);
 
-    tvGuestHits = (TextView) findViewById(R.id.tvGuestHits);
-    tvGuestHits.setOnTouchListener(new myTouchListener());
+        tvaInningsStats[k].setGravity(Gravity.CENTER);
+        tvaInningsStats[k+1].setGravity(Gravity.CENTER);
 
-    tvGuestErrors = (TextView) findViewById(R.id.tvGuestErrors);
-    tvGuestErrors.setOnTouchListener(new myTouchListener());
+        tvaInningsStats[k].setText(sInningStatsNames[k]);
+        tvaInningsStats[k+1].setText("0");
+        tvaInningsStats[k+1].setTextSize(TypedValue.COMPLEX_UNIT_DIP, InningTextSize);
 
-    tvHomeRuns = (TextView) findViewById(R.id.tvHomeRuns);
-    tvHomeRuns.setOnTouchListener(new myTouchListener());
+        tvaInningsStats[k].setTextColor(Color.CYAN);
+        tvaInningsStats[k+1].setTextColor(Color.BLACK);
 
-    tvHomeHits = (TextView) findViewById(R.id.tvHomeHits);
-    tvHomeHits.setOnTouchListener(new myTouchListener());
+        if (Build.VERSION.SDK_INT < 23) {
+          tvaInningsStats[k].setTextAppearance(this, android.R.style
+                  .TextAppearance_Large);
+          tvaInningsStats[k+1].setTextAppearance(this, android.R.style
+                  .TextAppearance_Large);
+        }
+        else {
+          tvaInningsStats[k].setTextAppearance(android.R.style
+                  .TextAppearance_DeviceDefault_Large);
+          tvaInningsStats[k+1].setTextAppearance(android.R.style
+                  .TextAppearance_DeviceDefault_Large);
+        }
 
-    tvHomeErrors = (TextView) findViewById(R.id.tvHomeErrors);
-    tvHomeErrors.setOnTouchListener(new myTouchListener());
+        tvaInningsStats[k].setTypeface(Typeface.DEFAULT_BOLD);
+        tvaInningsStats[k+1].setTypeface(Typeface.DEFAULT_BOLD);
 
-    {
-      int iId;
-      String sId;
-      for (int k=0; k < NumInnings; k++){
-        
+        llInningStats.addView(tvaInningsStats[k]);
+        llInningStats.addView(tvaInningsStats[k+1]);
+
       }
-
     }
-
   }
 
   // TODO: Crazy idea Change Guest and Home team names on touch
