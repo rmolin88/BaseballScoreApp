2e96388817d5e62e2a244abbb4861f616cb4be62
Reinaldo Molina <rmolin88@gmail.com>
2016-09-23 19:29:49 -0400

    good progreesss

--- app/src/main/java/com/hq/baseballscore/MainActivity.java
+++ app/src/main/java/com/hq/baseballscore/MainActivity.java
@@ -5,23 +5,24 @@ import android.graphics.Typeface;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
   private static final String TAG = "BaseballScoreApp";
   private static final int NumInnings = 9;
   private static final int NumStats = 6;
+  private static final int NumGameStats = 3;
+  private static final String ColorBlue = "#4689C8";
   private static final int InningTextSize = 64;
   private static final String[] sInningStatsNames = {"Ball", "Strike", "Out"};
-  private static final float llInningsStatsWeight = (float) 0.16;
+
+  private static final float llInningsStatsWeight = (float) 1/6;
 
   private static int[] iaGuestInningStats = new int[NumStats];
   private static int[] iaHomeInningStats = new int[NumStats];
@@ -32,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
   private static int[] iaGuestInningRuns = new int[NumInnings];
   private static int[] iaHomeInningRuns = new int[NumInnings];
 
-  private LinearLayout llInningStats;
+
   private TextView[] tvaInningsStats = new TextView[NumStats];
 
   private TextView[] tvaGuestGameStats = new TextView[NumInnings];
@@ -59,17 +60,17 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.content_scrolling);
 
-    llInningStats = (LinearLayout) findViewById(R.id.llInningStats);
-
+    // Setting up division 1
+    LinearLayout  llInningStats = (LinearLayout) findViewById(R.id.llInningStats);
     for (int k = 0, k1 = 0; k < NumStats; k++) {
       tvaInningsStats[k] = new TextView(this);
 
       // Even are words: balls, str....
       tvaInningsStats[k].setId(k);
       tvaInningsStats[k]
-          .setLayoutParams(
-              new LinearLayout.LayoutParams(
-                  0, LinearLayout.LayoutParams.MATCH_PARENT, llInningsStatsWeight));
+              .setLayoutParams(
+                      new LinearLayout.LayoutParams(
+                              0, LinearLayout.LayoutParams.MATCH_PARENT, llInningsStatsWeight));
       tvaInningsStats[k].setGravity(Gravity.CENTER);
       if (Build.VERSION.SDK_INT < 23) {
         tvaInningsStats[k].setTextAppearance(this, android.R.style.TextAppearance_Large);
@@ -84,10 +85,29 @@ public class MainActivity extends AppCompatActivity {
       } else {
         tvaInningsStats[k].setText(sInningStatsNames[k1++]);
         tvaInningsStats[k].setTypeface(Typeface.DEFAULT_BOLD);
-        tvaInningsStats[k].setTextColor(Color.CYAN);
+        tvaInningsStats[k].setTextColor(Color.parseColor(ColorBlue));
       }
 
       llInningStats.addView(tvaInningsStats[k]);
+    }
+
+    // Setting up Table 2
+    TableRow trTable1Row1 =(TableRow) findViewById(R.id.trTable1Row1);
+    for (int k = 0, k1 = NumGameStats; k < NumGameStats; k++) {
+      tvaGuestGameStats[k] = new TextView(this);
+
+      tvaGuestGameStats[k].setId(k1++);
+      tvaGuestGameStats[k].setGravity(Gravity.CENTER);
+      if (Build.VERSION.SDK_INT < 23) {
+        tvaGuestGameStats[k].setTextAppearance(this, android.R.style.TextAppearance_Large);
+      } else {
+        tvaGuestGameStats[k].setTextAppearance(android.R.style.TextAppearance_DeviceDefault_Large);
+      }
+      tvaGuestGameStats[k].setText("0");
+      tvaGuestGameStats[k].setTypeface(Typeface.DEFAULT_BOLD);
+      tvaGuestGameStats[k].setTextColor(Color.BLACK);
+
+      trTable1Row1.addView(tvaGuestGameStats[k]);
     }
   }
 
