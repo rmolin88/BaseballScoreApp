85d435d80beb69cff1e458697865529aa0a61915
Reinaldo Molina <rmolin88@gmail.com>
2016-09-30 08:06:33 -0400

    much better idea now of what is going on

--- app/src/main/java/com/hq/baseballscore/GameStats.java
+++ app/src/main/java/com/hq/baseballscore/GameStats.java
@@ -18,169 +18,169 @@ import android.widget.TableRow;
 import java.util.ArrayList;
 
 class GameStatsWrapper {
-  private GameStats gameStats;
-  private InningStats inningStats;
+	private GameStats gameStats;
+	private InningStats inningStats;
 
-  GameStatsWrapper(Activity activity_, Context context_) {
-    gameStats = new GameStats(activity_, context_);
-    inningStats = new InningStats(activity_, context_);
-  }
+	GameStatsWrapper(Activity activity_, Context context_) {
+		gameStats = new GameStats(activity_, context_);
+		inningStats = new InningStats(activity_, context_);
+	}
 
-  void UpdateFromInnings(GenTextView view) {
-    // if (iWho == 1) gameStats.tvHomeRuns.Update();
-    // else if (iWho == 0) {
-      // gameStats.tvGuestRuns.Update();
-    // } else throw new RuntimeException("Passed unrecognized value to function");
-  }
+	private void UpdateFromInnings(GenTextView view) {
+		// if (iWho == 1) gameStats.tvHomeRuns.Update();
+		// else if (iWho == 0) {
+		// gameStats.tvGuestRuns.Update();
+		// } else throw new RuntimeException("Passed unrecognized value to function");
 
-  void UpdateFromGameStats(GenTextView view) {
-    // TODO: Figure out how to determine if Correct team is at bat
+	}
 
-  }
+	private void UpdateFromGameStats(GenTextView view) {
+		// TODO: Figure out how to determine if Correct team is at bat
 
-  private class InningsTouchListener implements View.OnTouchListener {
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-      if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) { // If user pressed view
-        UpdateFromInnings((GenTextView) view);
-      }
-      return false;
-    }
-  }
+	}
 
-  private class GameStatsTouchListener implements View.OnTouchListener {
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-      if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) { // If user pressed view
-        UpdateFromGameStats((GenTextView) view);
-      }
-      return false;
-    }
-  }
-}
+	private class InningsTouchListener implements View.OnTouchListener {
+		public boolean onTouch(View view, MotionEvent motionEvent) {
+			if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) { // If user pressed view
+				UpdateFromInnings((GenTextView) view);
+			}
+			return false;
+		}
+	}
 
-class GameStats {
-  private static final int NUM_ELEMENTS = 3;
+	private class GameStatsTouchListener implements View.OnTouchListener {
+		public boolean onTouch(View view, MotionEvent motionEvent) {
+			if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) { // If user pressed view
+				UpdateFromGameStats((GenTextView) view);
+			}
+			return false;
+		}
+	}
 
-  GenTextView tvGuestRuns;
-  GenTextView tvHomeRuns;
+	private class GameStats {
+		private static final int NUM_ELEMENTS = 3;
 
-  private Activity activity;
+		GenTextView tvGuestRuns;
+		GenTextView tvHomeRuns;
 
-  GameStats(Activity activity_, Context context_) {
-    activity = activity_;
+		private Activity activity;
 
-    TableRow llGuestGameStats = (TableRow) activity_.findViewById(R.id.trTable1Row1);
-    TableRow llHomeGameStats = (TableRow) activity_.findViewById(R.id.trTable1Row2);
+		GameStats(Activity activity_, Context context_) {
+			activity = activity_;
 
-    for (int k = 0; k < NUM_ELEMENTS; k++) {
-      // Set first Guest elements
-      GenTextView tvGuest = new GenTextView(context_);
-      GenTextView tvHome = new GenTextView(context_);
+			TableRow llGuestGameStats = (TableRow) activity_.findViewById(R.id.trTable1Row1);
+			TableRow llHomeGameStats = (TableRow) activity_.findViewById(R.id.trTable1Row2);
 
-      tvGuest.setReset(10000); // Reset value like this so that it never happens
-      tvHome.setReset(10000); // Reset value like this so that it never happens
+			for (int k = 0; k < NUM_ELEMENTS; k++) {
+				// Set first Guest elements
+				GenTextView tvGuest = new GenTextView(context_);
+				GenTextView tvHome = new GenTextView(context_);
 
-      tvGuest.setGravity(Gravity.CENTER);
-      tvHome.setGravity(Gravity.CENTER);
+				tvGuest.setReset(10000); // Reset value like this so that it never happens
+				tvHome.setReset(10000); // Reset value like this so that it never happens
 
-      // Set appearance
-      if (Build.VERSION.SDK_INT < 23) {
-        tvGuest.setTextAppearance(activity, android.R.style.TextAppearance_DeviceDefault_Large);
-      } else {
-        tvGuest.setTextAppearance(android.R.style.TextAppearance_DeviceDefault_Large);
-      }
-      if (Build.VERSION.SDK_INT < 23) {
-        tvHome.setTextAppearance(activity, android.R.style.TextAppearance_DeviceDefault_Large);
-      } else {
-        tvHome.setTextAppearance(android.R.style.TextAppearance_DeviceDefault_Large);
-      }
+				tvGuest.setGravity(Gravity.CENTER);
+				tvHome.setGravity(Gravity.CENTER);
 
-      tvGuest.setText("0");
-      tvHome.setText("0");
+				// Set appearance
+				if (Build.VERSION.SDK_INT < 23) {
+					tvGuest.setTextAppearance(activity, android.R.style.TextAppearance_DeviceDefault_Large);
+				} else {
+					tvGuest.setTextAppearance(android.R.style.TextAppearance_DeviceDefault_Large);
+				}
+				if (Build.VERSION.SDK_INT < 23) {
+					tvHome.setTextAppearance(activity, android.R.style.TextAppearance_DeviceDefault_Large);
+				} else {
+					tvHome.setTextAppearance(android.R.style.TextAppearance_DeviceDefault_Large);
+				}
 
-      tvGuest.setTextColor(Color.BLACK);
-      tvHome.setTextColor(Color.BLACK);
+				tvGuest.setText("0");
+				tvHome.setText("0");
 
-      // TODO: how to create outter class OnTouchListener from here
-      tvGuest.setOnTouchListener(new GameStatsWrapper.
-      tvHome.setOnTouchListener(new GameStatsTouchListener());
+				tvGuest.setTextColor(Color.BLACK);
+				tvHome.setTextColor(Color.BLACK);
 
-      if (k == 0) { // Set pointers to important TextViews
-        tvGuestRuns = tvGuest;
-        tvHomeRuns = tvHome;
-      }
+				tvGuest.setOnTouchListener(new GameStatsTouchListener());
+				tvHome.setOnTouchListener(new GameStatsTouchListener());
 
-      llGuestGameStats.addView(tvGuest);
-      llHomeGameStats.addView(tvHome);
-    }
-  }
-}
+				if (k == 0) { // Set pointers to important TextViews
+					tvGuestRuns = tvGuest;
+					tvHomeRuns = tvHome;
+				}
 
-class InningStats {
-  private ArrayList<GenTextView> alGuestInnStats;
-  private ArrayList<GenTextView> alHomeInnStats;
-  private static final int NUM_ELEMENTS = 9;
+				llGuestGameStats.addView(tvGuest);
+				llHomeGameStats.addView(tvHome);
+			}
+		}
+	}
 
-  private Activity activity;
+	private class InningStats {
+		private ArrayList<GenTextView> alGuestInnStats;
+		private ArrayList<GenTextView> alHomeInnStats;
+		private static final int NUM_ELEMENTS = 9;
 
-  private GenTextView tvCurrInn;
+		private Activity activity;
 
-  InningStats(Activity activity_, Context context_) {
-    activity = activity_;
+		private GenTextView tvCurrInn;
 
-    alGuestInnStats = new ArrayList<>(NUM_ELEMENTS);
-    alHomeInnStats = new ArrayList<>(NUM_ELEMENTS);
+		InningStats(Activity activity_, Context context_) {
+			activity = activity_;
 
-    TableRow llGuestInnStats = (TableRow) activity_.findViewById(R.id.trTable0Row1);
-    TableRow llHomeInnStats = (TableRow) activity_.findViewById(R.id.trTable0Row2);
+			alGuestInnStats = new ArrayList<>(NUM_ELEMENTS);
+			alHomeInnStats = new ArrayList<>(NUM_ELEMENTS);
 
-    for (int k = 0; k < NUM_ELEMENTS; k++) {
-      GenTextView tvGuest = new GenTextView(context_);
-      GenTextView tvHome = new GenTextView(context_);
+			TableRow llGuestInnStats = (TableRow) activity_.findViewById(R.id.trTable0Row1);
+			TableRow llHomeInnStats = (TableRow) activity_.findViewById(R.id.trTable0Row2);
 
-      tvGuest.setReset(10000); // Reset value like this so that it never happens
-      tvHome.setReset(10000); // Reset value like this so that it never happens
+			for (int k = 0; k < NUM_ELEMENTS; k++) {
+				GenTextView tvGuest = new GenTextView(context_);
+				GenTextView tvHome = new GenTextView(context_);
 
-      tvGuest.setGravity(Gravity.CENTER);
-      tvHome.setGravity(Gravity.CENTER);
+				tvGuest.setReset(10000); // Reset value like this so that it never happens
+				tvHome.setReset(10000); // Reset value like this so that it never happens
 
-      // Set appearance
-      if (Build.VERSION.SDK_INT < 23) {
-        tvGuest.setTextAppearance(activity, android.R.style.TextAppearance_DeviceDefault_Large);
-      } else {
-        tvGuest.setTextAppearance(android.R.style.TextAppearance_DeviceDefault_Large);
-      }
-      if (Build.VERSION.SDK_INT < 23) {
-        tvHome.setTextAppearance(activity, android.R.style.TextAppearance_DeviceDefault_Large);
-      } else {
-        tvHome.setTextAppearance(android.R.style.TextAppearance_DeviceDefault_Large);
-      }
+				tvGuest.setGravity(Gravity.CENTER);
+				tvHome.setGravity(Gravity.CENTER);
 
-      tvGuest.setTextColor(Color.BLACK);
-      tvHome.setTextColor(Color.BLACK);
+				// Set appearance
+				if (Build.VERSION.SDK_INT < 23) {
+					tvGuest.setTextAppearance(activity, android.R.style.TextAppearance_DeviceDefault_Large);
+				} else {
+					tvGuest.setTextAppearance(android.R.style.TextAppearance_DeviceDefault_Large);
+				}
+				if (Build.VERSION.SDK_INT < 23) {
+					tvHome.setTextAppearance(activity, android.R.style.TextAppearance_DeviceDefault_Large);
+				} else {
+					tvHome.setTextAppearance(android.R.style.TextAppearance_DeviceDefault_Large);
+				}
 
-      tvGuest.setOnTouchListener(new InningsTouchListener());
-      tvHome.setOnTouchListener(new InningsTouchListener());
+				tvGuest.setTextColor(Color.BLACK);
+				tvHome.setTextColor(Color.BLACK);
 
-      // Set text only for the first inning and set as curr inning
-      if (k == 0) {
-        tvGuest.setText("0");
-        tvCurrInn = tvGuest;
-      }
+				tvGuest.setOnTouchListener(new InningsTouchListener());
+				tvHome.setOnTouchListener(new InningsTouchListener());
 
-      alGuestInnStats.add(tvGuest);
-      alHomeInnStats.add(tvHome);
+				// Set text only for the first inning and set as curr inning
+				if (k == 0) {
+					tvGuest.setText("0");
+					tvCurrInn = tvGuest;
+				}
 
-      llGuestInnStats.addView(tvGuest);
-      llHomeInnStats.addView(tvHome);
-    }
-  }
+				alGuestInnStats.add(tvGuest);
+				alHomeInnStats.add(tvHome);
 
-  private void UpdateView(GenTextView view) {
-    if (view == tvCurrInn) {
-      view.Update();
-      // Update Runs on Table1 as well
-      if (alGuestInnStats.contains(view)) super.UpdateFromInnings(0);
-      else UpdateFromInnings(1);
-    }
-  }
+				llGuestInnStats.addView(tvGuest);
+				llHomeInnStats.addView(tvHome);
+			}
+		}
+
+//		private void UpdateView(GenTextView view) {
+//			if (view == tvCurrInn) {
+//				view.Update();
+//				 Update Runs on Table1 as well
+//				if (alGuestInnStats.contains(view)) super.UpdateFromInnings(0);
+//				else UpdateFromInnings(1);
+//			}
+//		}
+	}
 }
